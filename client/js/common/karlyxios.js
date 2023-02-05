63d4e6ef33f2e5d556c6c8166d1f737c083d6c00
MIINII <ft.dawn.o@gmail.com>
2023-02-06 01:30:17 +0900

    Chore : kalyxios.js 수정

--- client/js/common/karlyxios.js
+++ client/js/common/karlyxios.js
@@ -1,4 +1,16 @@
-import { refError } from '/lib/error/refError.js';
+import { refError } from '/client/lib/error/refError.js';
+
+let body = {
+  id: '',
+  name: '',
+  password: '',
+  email: '',
+  phonenumber: '',
+  gender: 'n',
+  birth: '0000-00-00',
+  tos: '',
+  recommend: 0,
+};
 
 const defaultOptions = {
   method: 'GET',
@@ -10,31 +22,30 @@ const defaultOptions = {
   referrerPolicy: 'no-referrer',
   headers: {
     'Content-Type': 'application/json; charset=UTF-8',
-    'Project-Name': 'KarlyLion18-karly_fe'
-  }
-}
+    'Project-Name': 'KarlyLion18-karly_fe',
+  },
+};
 
 export const karlyxios = async (options = {}) => {
-
   const { url, ...restOptions } = {
-                                    ...defaultOptions,
-                                    ...options,
-                                    headers: {
-                                      ...defaultOptions.headers,
-                                      ...options.headers
-                                    }
+    ...defaultOptions,
+    ...options,
+    headers: {
+      ...defaultOptions.headers,
+      ...options.headers,
+    },
   };
 
   let response = await fetch(url, restOptions);
 
-  if(!response.ok){
-    refError("[karlyxios] : you were faild");
-  }else{
+  if (!response.ok) {
+    refError('[karlyxios] : you were faild');
+  } else {
     response.data = await response.json();
   }
 
   return response;
-}
+};
 
 /* -------------------------------------------------------------------------- */
 /*                  karlyxios 4 type (get, post, put, delete)                 */
@@ -43,32 +54,36 @@ export const karlyxios = async (options = {}) => {
 karlyxios.get = (url, options) => {
   return karlyxios({
     url,
-    ...options
+    ...options,
   });
 };
 
-karlyxios.post = (url, body, options) => {
-  return karlyxios({
-    url,
-    body:JSON.stringify(body),
-    method: 'POST',
-    ...options
-  });
+karlyxios.post = async (url, body, options) => {
+  try {
+    return await karlyxios({
+      url,
+      body: JSON.stringify(body),
+      method: 'POST',
+      ...options,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 karlyxios.put = (url, body, options) => {
   return karlyxios({
     url,
-    body:JSON.stringify(body),
+    body: JSON.stringify(body),
     method: 'PUT',
-    ...options
+    ...options,
   });
 };
 
 karlyxios.delete = (url, body, options) => {
   return karlyxios({
     url,
-    method: "DELETE",
-    ...options
+    method: 'DELETE',
+    ...options,
   });
 };
