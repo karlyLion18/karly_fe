266952f955e641fda0cbc94d1e38692cc699e149
MIINII <ft.dawn.o@gmail.com>
2023-02-06 01:27:42 +0900

    Feat : 데이터 전송하기 가능

--- client/js/account.js
+++ client/js/account.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { karlyxios } from "/client/js/common/karlyxios.js";
 import { getNode, attr, addClass, toggleClass, getInputValue, removeClass } from '/client/lib/index.js';
 
@@ -339,3 +340,55 @@ userBoxUserEmail.addEventListener('input',(e) => validationChekckEmail(e));
 userBoxUserPw.addEventListener('input', (e) => validationChekckPw(e));
 userBoxUserPwConfirm.addEventListener('input', (e) => validationChekckPwConfirm(e));
 userBoxUserPhone.addEventListener('input', (e) => validationChekckPhone(e));
+=======
+import { getNode } from '/client/lib/index.js';
+import { getInputValue } from '../lib/dom/getInputValue.js';
+import { karlyxios } from './common/karlyxios.js';
+
+let url = 'http://localhost:5001/users';
+
+const registerBtn = getNode('.register_btn');
+
+const userIdNode = getNode('.user__input-id');
+
+// console.log('🚀 ⁝ onSubmit ⁝ userGender', userGender);
+
+const onSubmit = () => {
+  const userID = getInputValue('.user__input-id');
+  const userPw = getInputValue('.user__input-pw');
+  const userName = getInputValue('.user__input-name');
+  const userEmail = getInputValue('.user__input-email');
+  const userPhone = getInputValue('.user__input-phone');
+  const userGender = document.querySelector('input[type="radio"]:checked').value;
+
+  const userBirth = () => {
+    const birthYear = getInputValue('.user__birth--year');
+    const birthMonth = getInputValue('.user__birth--month');
+    const birthDay = getInputValue('.user__birth--day');
+
+    return [birthYear, birthMonth, birthDay].join('.');
+  };
+
+  const data = {
+    id: userID,
+    name: userName,
+    password: userPw,
+    email: userEmail,
+    phonenumber: userPhone,
+    gender: userGender,
+    birth: userBirth(),
+    tos: '',
+    recommend: 0,
+  };
+
+  karlyxios.post(url, data);
+};
+
+registerBtn.addEventListener('click', () => {
+  if (onsubmit.data.value === '') {
+    alert(err);
+  } else {
+    onSubmit();
+  }
+});
+>>>>>>> 7008203 (Feat : 데이터 전송하기 가능)
